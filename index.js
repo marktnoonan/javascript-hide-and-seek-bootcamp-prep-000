ce579a0fd088de66c385cb8e9bbc5b17b521ea7d
Mark Noonan <markthomasnoonan@gmail.com>
2018-06-10 21:36:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ function deepestChild() {
   
   function goDeep(element){
     if (element.children.length){
+      // given that we are allowed to assume only one node at each level
       return goDeep(element.children[0])
     } else {
         return element        
     }
   }
-  
   return goDeep(grandNode)
-  
+}
+
+function getFirstSelector(selector){
+  return document.querySelector(selector)
 }
