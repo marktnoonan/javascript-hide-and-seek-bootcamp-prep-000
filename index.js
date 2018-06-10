c264b25c21da550ddae4b86868137dd9df8529e7
Mark Noonan <markthomasnoonan@gmail.com>
2018-06-10 21:41:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function deepestChild() {
+  const grandNode = document.querySelector("#grand-node")
+  
+  function goDeep(element){
+    if (element.children.length){
+      // given that we are allowed to assume only one node at each level
+      return goDeep(element.children[0])
+    } else {
+        return element        
+    }
+  }
+  return goDeep(grandNode)
+}
+
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector("#nested .target") 
+}
+
+function increaseRankBy(n) {
+  const rankedListItems = Array.from(document.querySelectorAll('.ranked-list > li'))
+  rankedListItems.forEach(item => {
+    item.textContent = parseInt(item.textContent) + n
+  })
+}
